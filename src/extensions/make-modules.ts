9f83de7cacc2f8bf5069a95855b7bc3348a889f1
jefeez <jefersonquerz@gmail.com>
2022-10-07 23:30:14 -0300

    fix

--- src/extensions/make-modules.ts
+++ src/extensions/make-modules.ts
@@ -4,12 +4,12 @@ import { GluegunToolbox } from 'gluegun'
 // to your commands
 module.exports = (toolbox: GluegunToolbox) => {
   toolbox.modules = async (name: string) => {
-    await toolbox.patching.patch(`./src/modules/modules.ts`, {
+    await toolbox.patching.patch(`./src/app/modules/modules.ts`, {
       insert: `\nimport ${name} from '@modules/${name}/router';`,
       after: new RegExp(`// imports`),
     })
 
-    await toolbox.patching.patch(`./src/modules/modules.ts`, {
+    await toolbox.patching.patch(`./src/app/modules/modules.ts`, {
       insert: `\nrouter.use('/${name}', ${name});`,
       after: new RegExp('// routers'),
     })
